14859ec7e3697edf5ef2f709753efaed85a56cf7
Heitor Lessa <heitor.lessa@hotmail.com>
2017-05-09 17:40:10 +0100

    Added hello User Group

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 const version string = "2.0.1"
+const msg string = "Hello User Group!!!"
 
 // VersionHandler handles incoming requests to /version
 // and just returns a simple version number
@@ -14,8 +15,13 @@ func versionHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, version)
 }
 
+func helloUG(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, msg)
+}
+
 func main() {
 	log.Printf("Listening on port 8000...")
 	http.HandleFunc("/version", versionHandler)
+	http.HandleFunc("/", helloUG)
 	http.ListenAndServe(":8000", nil)
 }
